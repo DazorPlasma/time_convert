79b630db29bce106ef45a36ece1674746c23b95a
DazorPlasma <dazorplasma@proton.me>
2023-05-27 22:39:58 +0300

    indent with spaces

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,30 @@ mod clocktime;
 use colored::Colorize;
 
 fn get_input() -> String {
-	let mut input: String = String::new();
-	std::io::stdin()
-		.read_line(&mut input)
-		.expect("Couldn't read input!");
-	return input;
+    let mut input: String = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Couldn't read input!");
+    return input;
 }
 
 fn main() {
-	let input: String = get_input();
+    let input: String = get_input();
 
-	let time = match clocktime::parse(&input) {
-		Ok(val) => val,
-		Err(err) => {
-			println!("{} {:?}", "ERROR:".red().bold(), err);
-			return;
-		}
-	};
+    let time = match clocktime::parse(&input) {
+        Ok(val) => val,
+        Err(err) => {
+            println!("{} {:?}", "ERROR:".red().bold(), err);
+            return;
+        }
+    };
 
-	println!("24h format: {}", time.to_string(clocktime::TimeFormat::TwentyFour));
-	println!("12h format: {}", time.to_string(clocktime::TimeFormat::Twelve));
+    println!(
+        "24h format: {}",
+        time.to_string(clocktime::TimeFormat::TwentyFour)
+    );
+    println!(
+        "12h format: {}",
+        time.to_string(clocktime::TimeFormat::Twelve)
+    );
 }
